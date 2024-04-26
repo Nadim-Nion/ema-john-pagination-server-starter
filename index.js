55fcb25683f2f46ab51fa472341375d63f9dbd38
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-04-26 23:21:06 +0600

    server side logic implemented for pagination

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qf8hqc8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 console.log(uri);
@@ -32,9 +32,36 @@ async function run() {
 
     const productCollection = client.db('emaJohnDB').collection('products');
 
+    // Get all products data
     app.get('/products', async (req, res) => {
-      const result = await productCollection.find().toArray();
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      console.log('Pagination Query', page, size);
+      const result = await productCollection.find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
       res.send(result);
+    })
+
+    // To load some data for Order route by their ids
+    app.post('/productByIds', async (req, res) => {
+      const ids = req.body;
+      const idsWithObjectId = ids.map(id => new ObjectId(id));
+      // console.log(idsWithObjectId);
+      const query = {
+        _id: {
+          $in: idsWithObjectId
+        }
+      };
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    // Count all products data
+    app.get('/productsCount', async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({ count });
     })
 
     // Send a ping to confirm a successful connection
